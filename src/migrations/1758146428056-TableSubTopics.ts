695a90b614be05fcfd5b2df9d90eea24e2194629
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-09-17 19:57:07 -0300

    Feature: adds the table migration files

--- src/migrations/1758146428056-TableSubTopics.ts
+++ src/migrations/1758146428056-TableSubTopics.ts
@@ -3,6 +3,14 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class TableSubTopics1758146428056 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            create table if not exists SubTopics(
+            "Id" serial primary key,
+            "TopicId" integer not null,
+            "SubTopicName" varchar(50) not null,
+            add constraint "Topic_fk" foreign key (TopicId) references "Topics"(Id)
+            )
+            `) 
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
--- src/migrations/1758146451813-TableQuestions.ts
+++ src/migrations/1758146451813-TableQuestions.ts
@@ -3,6 +3,27 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class TableQuestions1758146451813 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            create table if not exists Questions(
+            "Id" serial primary key,
+            "SubjectId" integer not null,
+            "TopicId" integer not null,
+            "SubTopicId" integer not null,
+            "ImageUrl" text,
+            "QuestionText" text,
+            "QuestionPrompt" text not null,
+            "QuestionAltA" text not null,
+            "QuestionAltB" text not null,
+            "QuestionAltC" text not null,
+            "QuestionAltD" text not null,
+            "QuestionAltE" text,
+            "CorrectAlternative" char,
+            "QuestionDifficulty" varchar(20),
+            constraint "SubjectFk" foreign key "SubjectId" references "Subjects"(id),
+            constraint "TopicFk" foreign key "TopicId" references "Topics"(id),
+            constraint "SubTopicFk" foreign key "SubTopicId" references "SubTopics"(id),
+            )
+            `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
