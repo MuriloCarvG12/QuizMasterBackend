100743a82089b9866e545db229434837826b1d90
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-09-17 19:02:11 -0300

    feature: initialzies the mgirations arhcives for setting up the tables

---
+++ src/migrations/1758146355205-TableSubjects.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TableSubjects1758146355205 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/migrations/1758146451813-TableQuestions.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TableQuestions1758146451813 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
