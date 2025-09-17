85eba7fd1969adde009e70006f270d4fa19f8d5b
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-09-17 18:55:41 -0300

    first commit

---
+++ src/data_source.ts
@@ -0,0 +1,19 @@
+
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD as string,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    logging: process.env.NODE_ENV === 'development' ? true : false ,
+    entities: [`src/entities/*.ts`],
+    migrations: [`src/migrations/*.ts`],
+    subscribers: [],
+    migrationsRun: process.env.NODE_ENV === "production" ? true : false
+})
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+import "reflect-metadata";
+import express from "express";
+import { AppDataSource } from "./data_source";
+
+import cors from "cors";
+
+const app = express();
+
+app.use(cors()); // Permite que o express entenda requisições de outros domínios
+
+app.use(express.json()); // Permite que o express entenda JSON
+
+
+
+app.get("/env", (req, res) => {
+  res.json({
+    port: process.env.PORT,
+    node_env: process.env.NODE_ENV,
+  });
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(`O servidor está rodando em http://localhost:3000`);
+    });
+  })
+  .catch((error) => {console.log("teste" + error)});
