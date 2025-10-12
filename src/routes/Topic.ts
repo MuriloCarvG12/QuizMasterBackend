7b4abfecad496e5e3d978de06bda3c59be04f285
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-10-12 12:42:01 -0300

    fix: fixes the createtopic route

--- src/routes/Topic.ts
+++ src/routes/Topic.ts
@@ -3,7 +3,7 @@ import { Router, Request , Response } from "express";
 import AppDataSource from "../data_source";
 
 import { Topic } from "../entities/Topic";
-import Subject from "./Subject";
+import {Subject} from "../entities/Subject";
 
 
 
@@ -35,7 +35,7 @@ topicRouter.get('/getTopic', async( req:Request, res: Response) => {
                 return res.status(400).json("Couldn't find a Topic with that code!");    
             }
 
-        const TopicFound = await TopicRepository.findBy({TopicName: ReqBodyName});
+        const TopicFound = await TopicRepository.findOneBy({TopicName: ReqBodyName});
         if(! TopicFound)
             {
                 return res.status(400).json("Couldn't find a topic with that name!");    
@@ -57,14 +57,15 @@ topicRouter.post('/createTopic', async( req:Request, res: Response) =>
     {
         const BodyTopicName = req.body.TopicName;
         const TopicSubject = req.body.SubjectId;   
-        const Subjectfound = await SubjectRepository.findBy({Id: TopicSubject});
+        const Subjectfound = await SubjectRepository.findOneBy({Id: TopicSubject});
 
         if(!Subjectfound)
              {
                 return res.status(400).json("Couldn't find a subject with that code!");    
             }
         
-        if(!TopicRepository.findBy({TopicName: BodyTopicName}))
+        const TopicFound = await TopicRepository.findOneBy({TopicName: BodyTopicName});
+        if(!TopicFound)
             {
                 const newTopic = TopicRepository.create
                 ({
