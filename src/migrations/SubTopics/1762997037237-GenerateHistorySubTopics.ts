b9b68d058eb94ebace5087c48474b7b7dc5348a1
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-11-13 20:39:43 -0300

    feature: sets up more topic migrations

--- src/migrations/SubTopics/1762997037237-GenerateHistorySubTopics.ts
+++ src/migrations/SubTopics/1762997037237-GenerateHistorySubTopics.ts
@@ -3,6 +3,15 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class GenerateHistorySubTopics1762997037237 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            INSERT INTO "Topics" ("SubjectId", "TopicName")
+                VALUES 
+                    (1, 'AncientAge')
+                    (2, 'MediumAge')
+                    (3, 'ModernAge')
+                    (3, 'ContemporaryAge')
+                    (3, 'Histografy')
+            `);  
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
--- src/migrations/SubTopics/1763075217585-OrganicChemistry.ts
+++ src/migrations/SubTopics/1763075217585-OrganicChemistry.ts
@@ -3,6 +3,15 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class OrganicChemistry1763075217585 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            INSERT INTO "Topics" ("SubjectId", "TopicName")
+                VALUES 
+                    (1, 'Introduction')
+                    (2, 'Isomery')
+                    (3, 'NitrogenFunctions')
+                    (3, 'OxygenatedFunctions')
+                    (3, 'HidroCarbons')
+            `);    
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
